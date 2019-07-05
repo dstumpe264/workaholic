93d88a4ac4981a86918cfee6db095a08942b31c1
David Stumpe <dstumpe264@gmail.com>
2019-07-05 14:53:53 -0400

    uploading project to git

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+
+  end
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  resources :users
+  root 'welcome#index'
+end
