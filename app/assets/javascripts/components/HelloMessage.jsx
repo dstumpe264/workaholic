5fc3ae45968f5cf9d8b5e9a0c2f645b24dd0abed
David Stumpe <dstumpe264@gmail.com>
2019-07-12 18:18:08 -0400

    added crud functions to user

--- app/assets/javascripts/components/HelloMessage.jsx
+++
@@ -1,9 +0,0 @@
-class HelloMessage extends React.Component({
-  render() {
-    return (
-      <div>
-        <h1>Hello {this.props.name}!</h1>
-      </div>
-    );
-  }
-};
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def update
+    @user = User.find(params[:id])
   end
 
   def destroy
   end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :pwd)
 
 end
