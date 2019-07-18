1e80ffee5a250752604d31d05638fa9253b40b9a
David Stumpe <dstumpe264@gmail.com>
2019-07-18 19:06:53 -0400

    began creating the skeleton for 'Job'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :users
+  resources :jobs
   root 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_233133) do
+ActiveRecord::Schema.define(version: 2019_07_18_230503) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
