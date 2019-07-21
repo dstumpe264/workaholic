8760850aaa29730b328c4a06bcc07e5b35c878ba
David Stumpe <dstumpe264@gmail.com>
2019-07-21 11:16:16 -0400

    added crud functions to jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,40 @@
 class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to @job
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @job = Job.find(params[:id])
+  end
+
+  def destroy
+  end
+
+private
+  def job_params
+    params.require(:job).permit(:title)
+  end
 end
